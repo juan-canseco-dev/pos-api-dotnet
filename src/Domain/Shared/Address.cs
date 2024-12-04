8dcd8b0ac97b945b3403d6049334b257d99214bf
Juan Pablo Canseco <juan.canseco.rios@gmail.com>
2024-12-03 19:13:02 -0600

    Add Shared folder to Domain

---
+++ src/Domain/Shared/Address.cs
@@ -0,0 +1,32 @@
+﻿using POS.Domain.Abstractions;
+
+namespace POS.Domain.Shared;
+
+public class Address : ValueObject
+{
+    public string Country { get; set; } = default!;
+    public string State { get; set; } = default!;
+    public string City { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+    public string Street { get; set; } = default!;
+
+    // Required By EF
+    private Address() { }
+    public Address(string country, string state, string city, string zipCode, string street)
+    {
+        Country = country;
+        State = state;
+        City = city;
+        ZipCode = zipCode;
+        Street = street;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Country;
+        yield return State;
+        yield return City;
+        yield return ZipCode;
+        yield return Street;
+    }
+}
