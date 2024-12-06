8495ec927d994cda86daec9cde2fcf120b31303d
Juan Pablo Canseco <juan.canseco.rios@gmail.com>
2024-12-06 09:03:56 -0600

    rename Name property to Description, add Type enum to Error record, add ErrorType property to Error

--- src/Domain/Abstractions/Error.cs
+++ src/Domain/Abstractions/Error.cs
@@ -1,7 +1,15 @@
 ﻿
 namespace POS.Domain.Abstractions;
-public record Error(string Code, string Name)
+public record Error(string Code, string Description, Error.Type ErrorType)
 {
-    public static Error None = new(string.Empty, string.Empty);
-    public static Error NullValue = new("Error.NullValue", "A null value was entered.");
+    public static Error None = new(string.Empty, string.Empty, Type.None);
+    public static Error NullValue = new("Error.NullValue", "A null value was entered.", Type.NullValue);
+
+    public enum Type
+    {
+        None = 0,
+        NullValue = 0,
+        NotFound = 1,
+        Domain = 2
+    }
 }
