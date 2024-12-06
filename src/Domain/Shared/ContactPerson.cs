367f1dba1faed3af8f502505de8f849a1687e7b4
Juan Pablo Canseco <juan.canseco.rios@gmail.com>
2024-12-06 08:19:18 -0600

    Add ContactPerson ValueObjet to Shared

---
+++ src/Domain/Shared/ContactPerson.cs
@@ -0,0 +1,28 @@
+﻿using POS.Domain.Abstractions;
+
+namespace POS.Domain.Shared;
+
+public class ContactPerson : ValueObject
+{
+    public string Fullname { get; }
+    public string Phone { get; }
+
+    // Required EF Core
+    private ContactPerson()
+    {
+        Fullname = default!;
+        Phone = default!;
+    }
+
+    public ContactPerson(string fullname, string phone)
+    {
+        Fullname = fullname;
+        Phone = phone;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Fullname;
+        yield return Phone;
+    }
+}
