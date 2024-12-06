75d04db7928a877705224d473b1c4d1095d53854
Juan Pablo Canseco <juan.canseco.rios@gmail.com>
2024-12-06 09:19:29 -0600

    Add Customers folder to Domain

---
+++ src/Domain/Customers/Customer.cs
@@ -0,0 +1,14 @@
+﻿using POS.Domain.Shared;
+
+namespace POS.Domain.Customers;
+
+public class Customer
+{
+    public string? IdentificationCode { get; set; }
+    public ContactPerson? ContactPerson { get; set; }
+    public Address? Address { get; set; }
+
+    private Customer() {}
+}
+
+
---
+++ src/Domain/Customers/CustomerErrors.cs
@@ -0,0 +1,34 @@
+﻿using POS.Domain.Abstractions;
+using POS.Domain.Shared;
+
+namespace POS.Domain.Customers;
+
+public static class CustomerErrors
+{
+    public static Error NotFound(int customerId)
+    {
+        return new Error(
+            Code: "Customer.NotFound",
+            Description: $"The specified Customer with the Id: {customerId} Was Not Found.",
+            ErrorType: Error.Type.NotFound
+        );
+    }
+
+    public static Error IdentificationCodeAlreadyExists(string identificationCode)
+    {
+        return new Error(
+          Code: "Customer.IdentificationCodeAlreadyExists",
+          Description: $"The specified IdentificationCode: {identificationCode} has already been used by another user.",
+          ErrorType: Error.Type.Domain
+      );
+    }
+
+    public static Error ContactPersonPhoneAlreadyExists(ContactPerson contactPerson)
+    {
+        return new Error(
+            Code: "Customer.CotactPerson.Phone.AlreadyExists",
+            Description: $"The specified ContactPerson.Phone: {contactPerson}  has already been used by another user.",
+            ErrorType: Error.Type.Domain
+        );
+    }
+}
