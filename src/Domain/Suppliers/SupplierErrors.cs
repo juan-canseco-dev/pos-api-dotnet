d866b482354208de1cc00d0001377251666bcf68
Juan Pablo Canseco <juan.canseco.rios@gmail.com>
2024-12-06 11:40:41 -0600

    Fix error messages in SupplierErrors to improve clarity and consistency.

--- src/Domain/Suppliers/SupplierErrors.cs
+++ src/Domain/Suppliers/SupplierErrors.cs
@@ -7,7 +7,7 @@ public static class SupplierErrors
     public static Error NotFound(int supplierId)
     {
         return new Error(
-            Code: "SupplierError.NotFound",
+            Code: "Supplier.NotFound",
             Description: $"The specified Supplier with the Id: {supplierId} Was Not Found.",
             ErrorType: Error.Type.NotFound
         );
@@ -16,7 +16,7 @@ public static class SupplierErrors
     public static Error PhoneNumberAlreadyExists(string phone)
     {
         return new Error(
-            Code: "Company.Phone",
+            Code: "Supplier.Phone.AlreadyExists",
             Description: $"The specified Phone number: {phone} has already been used by another user.",
             ErrorType: Error.Type.Domain
         );
